02c27a4fb09a45791071d1109c785f216b6bc092
Gpaqsa <giorgi.paksashvili.2@btu.edu.ge>
2023-02-19 10:51:03 +0400

    Erorr

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import Data from "./MOCK_DATA.json";
+import { useState } from 'react';
 
-function App() {
+
+const App = () => {
+  const { searchName, setSearchName } = useState("");
+  const { filterData, setFilterData } = useState([])
+
+  const handlerFilter = (event) => {
+    const searchName = event.target.value;
+    setSearchName(searchName);
+
+    const newNameFilter = Data.filter((value) => {
+      return (value.first_name.toLowerCase().includes(searchName.toLowerCase()))
+    });
+
+    if (searchName === " ") {
+      setFilterData([])
+    } else {
+      setFilterData(newNameFilter);
+    };
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <section className='search-box'>
+        <input
+          value={searchName}
+          onChange={handlerFilter}
+          placeholder="Search for name"
+        />
+      </section>
+
+      <section className='names-container'>
+
+        <div className='empty'>
+          <h1>Name Data</h1>
+          <hr />
+          <div className='names'>
+            {Data.map((value, key) => {
+              return (<div key={key}>
+                <p >{value.first_name}</p>
+              </div>)
+            })}
+          </div>
+        </div>
+
+      </section>
     </div>
   );
 }
